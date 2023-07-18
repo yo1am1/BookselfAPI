aedf82df54e02d27d377d2e86c67c687243b8f56
yo1am1 <bigdiebam@gmail.com>
2023-07-18 03:20:31 +0300

    .

--- book_shelf/views.py
+++ book_shelf/views.py
@@ -57,12 +57,7 @@ class BookView(View):
             books = Book.objects.filter(filters)
 
             if not books:
-                raise Book.DoesNotExist and JsonResponse(
-                    {
-                        "message": "No books found",
-                        "status": 404,
-                    }
-                )
+                raise Book.DoesNotExist
 
             books_data = serialize(
                 "json",
