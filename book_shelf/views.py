e2d23ac10c0685283f083576e59d93db81acf95c
yo1am1 <bigdiebam@gmail.com>
2023-07-18 03:49:58 +0300

    .

--- book_shelf/views.py
+++ book_shelf/views.py
@@ -57,7 +57,7 @@ class BookView(View):
             books = Book.objects.filter(filters)
 
             if not books:
-                return HttpResponse(
+                return JsonResponse(
                     {
                         "message": "No books found",
                         "status": 404,
