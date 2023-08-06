644408e0025b269866f6fa26071c7e059dcdc9d2
yo1am1 <bigdiebam@gmail.com>
2023-08-07 02:22:48 +0300

    Status check

--- apiv3/serializers.py
+++ apiv3/serializers.py
@@ -84,7 +84,7 @@ class OrderSerializer(serializers.Serializer):
 class OrderModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ["total_price", "created_at", "invoice_id", "id", "books"]
+        fields = ["total_price", "created_at", "invoice_id", "id", "books", "status"]
 
 
 class MonoCallbackSerializer(serializers.Serializer):
