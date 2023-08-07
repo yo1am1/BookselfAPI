d4d064cd374fc70c8668781a892232a021c18977
Yegor Trepalin <110065476+yo1am1@users.noreply.github.com>
2023-08-07 16:13:46 +0300

    Update e2e_tests.py

--- book_shelf/e2e_tests.py
+++ book_shelf/e2e_tests.py
@@ -3,10 +3,10 @@ import os
 import pytest
 import requests
 
-BASE_URL = "https://boiling-dusk-49835-df388a71925c.herokuapp.com/"
+BASE_URL = "https://cryptic-river-21647-7efe93940f14.herokuapp.com/"
 
 
-@pytest.mark.xfail
+# @pytest.mark.xfail
 def test_post_and_id_with_get_put_remove():
     global book_id
     url = BASE_URL + "books/"
@@ -26,60 +26,73 @@ def test_post_and_id_with_get_put_remove():
     assert response.status_code == 201
 
 
-def test_get_put_delete():
+def test_get_book_by_id():
+    global book_id
     response = requests.get(f"{BASE_URL}books/{book_id}")
 
     assert response.status_code == 200
-    print(response.json()[0])
     assert response.json()[0]["fields"]["title"] == "The Great Gatsby"
-    assert (
-        response.json()[0]["fields"]["author"]
-        == "(<Author: F. Scott Fitzgerald>, False)"
-    )
+    assert response.json()[0]["fields"]["author"]
     assert response.json()[0]["fields"]["genre"] == "Classic"
     assert response.json()[0]["fields"]["publish_year"] == 1925
 
+
+def test_put_book_by_id():
+    global book_id
+
     data = {
         "title": "The Great Gatsby 2",
-        "author": "F. Scott Fitzgerald 2",
-        "genre": "Classic 2",
-        "publish_year": 19252,
+        "author": "F. Scott Fitzgerald",
+        "genre": "Classic",
+        "publish_year": "1925",
     }
     response = requests.put(
         f"{BASE_URL}books/{book_id}",
         json=data,
         headers={"Content-Type": "application/json"},
     )
-    print(response.json())
-    assert response.status_code == 200
-    assert response.json()["message"] == "Book 'The Great Gatsby 2' has been updated"
-    assert response.json()["updated_fields"] == [
-        "title",
-        "publish_year",
-        "author",
-        "genre",
-    ]
 
-    response = requests.delete(
-        f"{BASE_URL}books/{book_id}", headers={"Content-Type": "application/json"}
-    )
-    print(response.json())
+    assert response.status_code == 200
+    assert response.json()["message"] == f"Book '{data['title']}' has been updated"
+
+
+def test_remove_book_by_id():
+    global book_id
+    response = requests.delete(f"{BASE_URL}books/{book_id}")
+
     assert response.status_code == 200
     assert response.json()["book"] == "'The Great Gatsby 2' has been deleted"
-    assert response.json()["status"] == 200
+
+
+def test_put_book_by_id_deleted():
+    global book_id
+
+    data = {
+        "title": "The Great Gatsby 2",
+        "author": "F. Scott Fitzgerald",
+        "genre": "Classic",
+        "publish_year": "1925",
+    }
+    response = requests.put(
+        f"{BASE_URL}books/{book_id}",
+        json=data,
+        headers={"Content-Type": "application/json"},
+    )
+
+    print(response.json())
+    assert response.status_code == 400
+    assert response.json()["Error"] == "Book does not exist"
 
 
 def test_books_get():
     r = requests.get(BASE_URL + "books/")
     assert r.status_code == 200
 
-    r = requests.get(BASE_URL + "books/?title=The Adventures of Sherlock Holmes")
+    r = requests.get(BASE_URL + "books/?search=The Adventures of Sherlock Holmes")
     assert r.status_code == 200
 
-    r = requests.get(
-        BASE_URL + "books/?title=The Adventures of Sherlock Holmes&author=J"
-    )
-    assert r.status_code == 404
+    r = requests.get(BASE_URL + "books/?search=J")
+    assert r.status_code == 200
 
     r = requests.get(
         BASE_URL + "books/?title=The Adventures of Sherlock Holmes&genre=Novel"
@@ -99,8 +112,8 @@ def test_books_get():
     assert r.status_code == 404
 
     r = requests.get(
-        BASE_URL
-        + "books/?title=The Adventures of Sherlock Holmes&author=Arthur Conan Doyle&genre=Novel&publish_year=1892&publish_year=1893"
+        BASE_URL + "books/?title=The Adventures of Sherlock Holmes&author=Arthur Conan "
+        "Doyle&genre=Novel&publish_year=1892&publish_year=1893"
     )
     assert r.status_code == 404
 
