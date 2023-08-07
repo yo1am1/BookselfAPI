22cc51392864b84d03b2573fbbd9fe4bb6b68f91
Yegor Trepalin <110065476+yo1am1@users.noreply.github.com>
2023-08-07 18:45:47 +0300

    Update test_apiv3.py

--- apiv3/test_apiv3.py
+++ apiv3/test_apiv3.py
@@ -19,12 +19,12 @@ def api_client():
 
 @pytest.fixture
 def author():
-    return Author.objects.create(name='Test Author')
+    return Author.objects.create(name="Test Author")
 
 
 @pytest.fixture
 def user():
-    return User.objects.create_user(username='testuser', password='testpassword244685')
+    return User.objects.create_user(username="testuser", password="testpassword244685")
 
 
 @pytest.fixture
@@ -42,52 +42,52 @@ def authenticated_client(api_client, access_token):
 @pytest.fixture
 def book(author):
     return Book.objects.create(
-        title='Test Book',
-        genre='Fiction',
+        title="Test Book",
+        genre="Fiction",
         author=author,
         publish_year=2023,
         price=20,
-        amount=10
+        amount=10,
     )
 
 
 @pytest.mark.django_db
 def test_urls_detail(api_client):
-    user = User.objects.create_user(username='testuser', password='testpassword')
+    user = User.objects.create_user(username="testuser", password="testpassword")
     token, created = Token.objects.get_or_create(user=user)
 
     api_client = APIClient()
-    api_client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
+    api_client.credentials(HTTP_AUTHORIZATION=f"Token {token.key}")
 
-    url = reverse('urls-detail')
+    url = reverse("urls-detail")
     response = api_client.get(url)
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
 
-    assert 'disclaimer' in data[0]
-    assert 'authors' in data[1]
-    assert 'books' in data[1]
-    assert 'detailed' in data[1]['authors']
-    assert 'update' in data[1]['authors']
-    assert 'delete' in data[1]['authors']
+    assert "disclaimer" in data[0]
+    assert "authors" in data[1]
+    assert "books" in data[1]
+    assert "detailed" in data[1]["authors"]
+    assert "update" in data[1]["authors"]
+    assert "delete" in data[1]["authors"]
 
-    assert 'list' in data[1]['books']
-    assert 'create' in data[1]['books']
-    assert 'detailed' in data[1]['books']
-    assert 'update' in data[1]['books']
-    assert 'delete' in data[1]['books']
+    assert "list" in data[1]["books"]
+    assert "create" in data[1]["books"]
+    assert "detailed" in data[1]["books"]
+    assert "update" in data[1]["books"]
+    assert "delete" in data[1]["books"]
 
-    assert 'register' in data[1]['users']
-    assert 'login' in data[1]['users']
+    assert "register" in data[1]["users"]
+    assert "login" in data[1]["users"]
 
-    assert 'order' in data[1]['mono']
-    assert 'order_detailed' in data[1]['mono']
-    assert 'order_list' in data[1]['mono']
+    assert "order" in data[1]["mono"]
+    assert "order_detailed" in data[1]["mono"]
+    assert "order_list" in data[1]["mono"]
 
 
 @pytest.mark.django_db
 def test_author_list(api_client, author):
-    url = reverse('author-list')
+    url = reverse("author-list")
     response = api_client.get(url)
     assert response.status_code == status.HTTP_200_OK
     assert len(response.data) > 0
@@ -95,69 +95,75 @@ def test_author_list(api_client, author):
 
 @pytest.mark.django_db
 def test_book_detail(api_client, book):
-    url = reverse('book-detail', args=[book.id])
+    url = reverse("book-detail", args=[book.id])
     response = api_client.get(url)
     print(response.data)
     assert response.status_code == status.HTTP_200_OK
-    assert response.data['title'] == 'Test Book'
-    assert response.data['author'] == 1
+    assert response.data["title"] == "Test Book"
+    assert response.data["author"] == 1
 
 
 @pytest.mark.django_db
 def test_book_create_unauthorized(api_client):
-    url = reverse('book-create')
+    url = reverse("book-create")
     data = {
-        'title': 'New Book',
-        'genre': 'Mystery',
-        'author': {'name': 'New Author'},
-        'publish_year': 2022,
-        'price': 25,
-        'amount': 5
+        "title": "New Book",
+        "genre": "Mystery",
+        "author": {"name": "New Author"},
+        "publish_year": 2022,
+        "price": 25,
+        "amount": 5,
     }
-    response = api_client.post(url, data, format='json')
+    response = api_client.post(url, data, format="json")
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert not Book.objects.filter(title='New Book').exists()
+    assert not Book.objects.filter(title="New Book").exists()
 
 
 @pytest.mark.django_db
 def test_author_update_unauthorized(api_client, author):
-    url = reverse('author-update', args=[author.id])
-    data = {'name': 'Updated Author Name'}
-    response = api_client.put(url, data, format='json')
+    url = reverse("author-update", args=[author.id])
+    data = {"name": "Updated Author Name"}
+    response = api_client.put(url, data, format="json")
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert not Author.objects.get(id=author.id).name == 'Updated Author Name'
+    assert not Author.objects.get(id=author.id).name == "Updated Author Name"
 
 
 @pytest.mark.django_db
 def test_book_create_authorized(authenticated_client, author):
-    url = reverse('book-create')
+    url = reverse("book-create")
     data = {
-        'title': 'New Book',
-        'genre': 'Mystery',
-        'author': author.id,
-        'publish_year': 2022,
-        'price': 25,
-        'amount': 5
+        "title": "New Book",
+        "genre": "Mystery",
+        "author": author.id,
+        "publish_year": 2022,
+        "price": 25,
+        "amount": 5,
     }
-    response = authenticated_client.post(url, data, format='json')
+    response = authenticated_client.post(url, data, format="json")
     assert response.status_code == status.HTTP_201_CREATED
-    assert Book.objects.filter(title='New Book').exists()
+    assert Book.objects.filter(title="New Book").exists()
 
 
 @pytest.mark.django_db
 def test_book_update_authorized(authenticated_client, book):
-    url = reverse('book-update', args=[book.id])
-    data = {'title': 'Updated Book Title', 'genre': 'Mystery', 'publish_year': 2022, 'price': 25, 'amount': 5,
-            'author': 1}
-    response = authenticated_client.put(url, data, format='json')
+    url = reverse("book-update", args=[book.id])
+    data = {
+        "title": "Updated Book Title",
+        "genre": "Mystery",
+        "publish_year": 2022,
+        "price": 25,
+        "amount": 5,
+        "author": 1,
+    }
+    response = authenticated_client.put(url, data, format="json")
     print(response.data)
     assert response.status_code == status.HTTP_200_OK
-    assert Book.objects.get(id=book.id).title == 'Updated Book Title'
+    assert Book.objects.get(id=book.id).title == "Updated Book Title"
 
 
 @pytest.mark.django_db
 def test_book_delete_authorized(authenticated_client, book):
-    url = reverse('book-delete', args=[book.id])
+    url = reverse("book-delete", args=[book.id])
     response = authenticated_client.delete(url)
     assert response.status_code == status.HTTP_204_NO_CONTENT
     assert not Book.objects.filter(id=book.id).exists()
@@ -165,73 +171,73 @@ def test_book_delete_authorized(authenticated_client, book):
 
 @pytest.mark.django_db
 def test_author_creation():
-    author = Author.objects.create(name='Test Author')
+    author = Author.objects.create(name="Test Author")
     assert author.id is not None
-    assert str(author) == 'Test Author'
+    assert str(author) == "Test Author"
 
 
 @pytest.mark.django_db
 def test_author_remove():
-    author = Author.objects.create(name='Test Author')
+    author = Author.objects.create(name="Test Author")
     author.delete()
-    assert not Author.objects.filter(name='Test Author').exists()
+    assert not Author.objects.filter(name="Test Author").exists()
 
 
 @pytest.mark.django_db
 def test_book_creation(author):
     book = Book.objects.create(
-        title='Test Book',
-        genre='Fiction',
+        title="Test Book",
+        genre="Fiction",
         author=author,
         publish_year=2023,
         price=20,
-        amount=10
+        amount=10,
     )
     assert book.id is not None
-    assert str(book) == 'Test Book'
+    assert str(book) == "Test Book"
 
 
 @pytest.mark.django_db
 def test_book_remove(author):
     book = Book.objects.create(
-        title='Test Book',
-        genre='Fiction',
+        title="Test Book",
+        genre="Fiction",
         author=author,
         publish_year=2023,
         price=20,
-        amount=10
+        amount=10,
     )
     book.delete()
-    assert not Book.objects.filter(title='Test Book').exists()
+    assert not Book.objects.filter(title="Test Book").exists()
 
 
 @pytest.mark.django_db
 def test_book_serializer():
-    author = Author.objects.create(name='Test Author')
+    author = Author.objects.create(name="Test Author")
     data = {
-        'title': 'Test Book',
-        'genre': 'Fiction',
-        'author': author.id,
-        'publish_year': 2023,
-        'price': 20,
-        'amount': 10
+        "title": "Test Book",
+        "genre": "Fiction",
+        "author": author.id,
+        "publish_year": 2023,
+        "price": 20,
+        "amount": 10,
     }
     serializer = BookSerializer(data=data)
     assert serializer.is_valid()
     book = serializer.save()
     assert book.id is not None
-    assert str(book) == 'Test Book'
+    assert str(book) == "Test Book"
 
 
 @pytest.mark.django_db
 def test_book_list_view():
     client = APIClient()
-    url = reverse('book-list')
+    url = reverse("book-list")
     response = client.get(url)
 
     assert response.status_code == status.HTTP_200_OK
     assert len(response.data) == 4
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pytest.main([os.path.realpath(__file__)])
